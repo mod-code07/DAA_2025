755111a32b7e1528910914818ec94a908dac577d
mod-code07 <samudranil.dutta3110@gmail.com>
2025-01-31 15:38:16 +0530

    merge sort 31 jan

---
+++ Section M Practical/Lec 4/mergeSort.java
@@ -0,0 +1,14 @@
+public class mergeSort {
+
+    
+
+    public static void main(String args[]) {
+        int arr[] = {6, 4, 2, 1, 9, 8, 3, 5};
+        int n = arr.length;
+
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
